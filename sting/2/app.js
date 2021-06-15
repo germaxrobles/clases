b0193c6a023b79e0e1a0cecdb75bfb779ff572bc
Gerardo Maximiliano Robles <germaxrobles@gmail.com>
2021-06-15 17:35:31 -0300

    15/6

---
+++ sting/2/app.js
@@ -0,0 +1,9 @@
+let nombre="Eugenio"
+let apellido="Gonzalez"
+let frase= "Bienbenido a la clase"
+
+console.log(nombre, apellido)
+console.log(nombre + "  " + apellido)
+
+//template String
+console.log(`Mi nombre es ${nombre} ${apellido}`)
